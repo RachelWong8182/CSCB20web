86a868fc8b08271326467b2acbfd84032a276789
Cindy Xing <jinqingxing22@gmail.com>
2026-03-14 16:10:58 -0400

    merge with Rachel

--- web/app.py
+++ web/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='html', static_folder='.', static_url_path='')
 
 @app.route('/')
 def mainpage():
-    return render_template('login.html')
+    return render_template('mainpage.html')
 
 app.run(debug = True)
