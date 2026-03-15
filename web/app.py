9d4d64f6f594267743455f3ce60b70316eadc91d
RachelWong8182 <rachelwong8182@gmail.com>
2026-03-15 05:08:00 -0400

    Finished basic login design

--- web/app.py
+++ web/app.py
@@ -6,4 +6,8 @@ app = Flask(__name__, template_folder='html', static_folder='.', static_url_path
 def mainpage():
     return render_template('mainpage.html')
 
-app.run(debug = True)
+@app.route('/pop_up.html')
+def popup():
+    return render_template('pop_up.html')
+
+app.run(debug=True)
--- web/java/login_window.js
+++ web/java/login_window.js
@@ -1,6 +1,6 @@
 function login_popup(){
-    fetch('main_page.html')
-        .when(rawfile => rawfile.text())
+    fetch('pop_up.html')
+        .then(rawfile => rawfile.text())
         .then(html => {
         document.getElementById('modal-container').innerHTML = html;
         document.getElementById('login-popup').style.display = 'flex';
