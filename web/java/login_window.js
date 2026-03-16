b4bc3569f0212bab883c8ae80253af3536dd4fd9
RachelWong8182 <rachelwong8182@gmail.com>
2026-03-15 21:03:03 -0400

    Register buttonadded

--- web/java/login_window.js
+++ web/java/login_window.js
@@ -6,3 +6,11 @@ function login_popup(){
         document.getElementById('login-popup').style.display = 'flex';
         });
 }
+
+function register_popup(){
+    fetch('pop_up.html')
+    .then(rawfile => rawfile.text())
+    .then(html => {
+    document.getElementById("registerForm").style.display = "block";
+    });
+}
